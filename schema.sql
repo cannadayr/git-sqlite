1072a0d9c260bbc36bc377a45e0177fb99092aa8
Rowan Cannaday <cannadayr@gmail.com>
2017-05-27 16:55:47 -0700

    added 'baz' schema w/ psuedo ad-hoc uuid

--- schema.sql
+++ schema.sql
@@ -3,3 +3,21 @@ CREATE TABLE bar (
     name text,
     time_added timestamp default current_timestamp
 );
+
+CREATE TABLE baz (
+    id integer primary key,
+    "id" char(36) default (
+        lower(hex(randomblob(4)))
+            || '-'
+            || lower(hex(randomblob(2)))
+            || '-4'
+            || substr(lower(hex(randomblob(2))),2)
+            || '-'
+            || substr('89ab',abs(random()) % 4 + 1, 1)
+            || substr(lower(hex(randomblob(2))),2)
+            || '-'
+            || lower(hex(randomblob(6)))
+    ),
+    name text,
+    time_added timestamp default current_timestamp
+);
