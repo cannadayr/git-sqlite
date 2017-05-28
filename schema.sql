b56925cb15b9f899e37bc0e67598ef2debe06a56
Rowan Cannaday <cannadayr@gmail.com>
2017-05-28 14:47:53 -0500

    rm'ed 'charlie' tbl from sql schema

--- schema.sql
+++ schema.sql
@@ -5,24 +5,6 @@
 -- that this pseudo, ad-hoc uuid
 -- won't produce a collision
 -- so we won't need a primary key
-CREATE TABLE charlie (
-    "id" char(36) default (
-        lower(hex(randomblob(4)))
-            || '-'
-            || lower(hex(randomblob(2)))
-            || '-4'
-            || substr(lower(hex(randomblob(2))),2)
-            || '-'
-            || substr('89ab',abs(random()) % 4 + 1, 1)
-            || substr(lower(hex(randomblob(2))),2)
-            || '-'
-            || lower(hex(randomblob(6)))
-    ),
-    name text,
-    time_added timestamp default current_timestamp,
-    primary key(id)
-) WITHOUT ROWID;
-
 CREATE TABLE entity (
     "id" char(36) default (
         lower(hex(randomblob(4)))
