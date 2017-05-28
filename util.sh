a9d087686465eaf69e0646631d62898301d4411d
Rowan Cannaday <cannadayr@gmail.com>
2017-05-27 21:25:51 -0700

    changed some vars to be closer to git args

--- util.sh
+++ util.sh
@@ -7,9 +7,9 @@ printErr()
 
 diffDb()
 {
-    current="$1"
-    checked_in="$2"
+    localDb="$1"
+    tmpDb="$2"
     path="$trunk/modules/sqlite3-3.16.2"
-    $path/sqldiff "$current" "$checked_in"
+    $path/sqldiff "$localDb" "$tmpDb"
 }
 
