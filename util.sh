0495af323f6fe204f9cd7adf27d8dd2ae8f8a2fb
Rowan Cannaday <cannadayr@gmail.com>
2017-05-28 04:13:43 -0500

    comments on diffDb

--- util.sh
+++ util.sh
@@ -7,6 +7,13 @@ printErr()
 
 diffDb()
 {
+    # NOTE: sqldiff currently can't perform diffs
+    # on views or triggers
+    # see: https://sqlite.org/sqldiff.html#limitations
+    # HOWEVER we might be able to get around this using
+    #   sqldiff --tables sqlite_master
+    # AND if we add a UUID to the sqlite_master table as a
+    # primary key
     localDb="$1"
     tmpDb="$2"
     path="$trunk/modules/sqlite3-3.16.2"
