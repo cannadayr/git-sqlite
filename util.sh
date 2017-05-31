6b4057b5883faf2ebbe73c51e25a8becffdcb377
ryanfmurphy <ryanfmurphy@gmail.com>
2017-05-30 23:24:31 -0500

    (awkward) shims for Mac OS X compatibility
    instead of looking for /tmp/ in the git diff args
    just look for a leading /
    
    use a different sqlite-build path
    
    use GNU coreutils greadlink as a swap-in for readlink

--- util.sh
+++ util.sh
@@ -18,7 +18,15 @@ diffDb()
     # see: https://sqlite.org/sessionintro.html
     localDb="$1"
     tmpDb="$2"
-    path="$trunk/modules/sqlite3-3.16.2"
+    #todo #fixme On Mac OS X I had to use the original SQLite source
+    #            instead of the Debian package included as /sqlite3-3.16.2
+    #            Let's decide on a unified path: I nominate /sqlite-build.
+    #               ~ RM
+    if isDarwin; then
+        path="$trunk/modules/sqlite-build"
+    else
+        path="$trunk/modules/sqlite3-3.16.2"
+    fi
     $path/sqldiff --transaction "$localDb" "$tmpDb"
 }
 
