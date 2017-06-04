5f7d275a7495209cdf34785a6e184a0352c61d6c
ryanfmurphy <ryanfmurphy@gmail.com>
2017-06-03 23:38:25 -0500

    initial db_config.php for db_viewer

---
+++ extras/db_config.php
@@ -0,0 +1,16 @@
+<?php
+    $git_sqlite_trunk = dirname(__FILE__);
+    $db_name = "$git_sqlite_trunk/net.db";
+    $id_mode = 'id_only';
+    $fields_to_make_selects = array(
+        'entity_id',
+        'tag_id',
+    );
+    $minimal_fields_by_table = array(
+        'tag2entity' => array(
+            'id',
+            'time_added',
+            'tag_id',
+            'entity_id',
+        ),
+    );
